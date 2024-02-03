6915c4aeb553272d25d0ea4c3a518e0435063a72
Adnan Arodiya <aiarodiya07@gmail.com>
2024-02-03 11:00:05 +0530

    signing a new user

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import AuthForm from "../AuthForm";
+
 export default function Login() {
+  const handleSubmit = async (
+    e: { preventDefault: () => void },
+    email: any,
+    password: any
+  ) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  };
+
   return (
     <main>
       <h2 className="text-center">Login</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,45 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import AuthForm from "../AuthForm";
+import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
 export default function Signup() {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (
+    e: { preventDefault: () => void },
+    email: any,
+    password: any
+  ) => {
+    e.preventDefault();
+
+    setError("");
+
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`,
+      },
+    });
+    if (error) {
+      setError(error.message);
+    }
+    if (!error) {
+      router.push("/verify");
+    }
+    console.log(email, password);
+  };
+
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
+      <AuthForm handleSubmit={handleSubmit} />
+      {error && <div className="error">{error}</div>}
     </main>
   );
 }
