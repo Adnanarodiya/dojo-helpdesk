6763836e0b5bfae6927745809c67a25148a15c8d
Adnan Arodiya <aiarodiya07@gmail.com>
2024-02-04 13:31:57 +0530

    intro to server component

--- app/(dashboard)/tickets/create/CreateForm.tsx
+++ app/(dashboard)/tickets/create/CreateForm.tsx
@@ -1,7 +1,7 @@
 "use client";
-
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { addTicket } from "../action";
 
 export default function CreateForm() {
   const router = useRouter();
@@ -15,11 +15,7 @@ export default function CreateForm() {
     e.preventDefault();
     setIsLoading(true);
 
-    const newTicket = {
-      title,
-      body,
-      priority,
-    };
+    const newTicket = { title, body, priority };
 
     const res = await fetch("http://localhost:3000/api/tickets", {
       method: "POST",
@@ -33,13 +29,13 @@ export default function CreateForm() {
       console.log("error.message");
     }
     if (json.data) {
-      router.push("/tickets");
       router.refresh();
+      router.push("/tickets");
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-1/2">
+    <form action={addTicket} className="w-1/2">
       <label>
         <span>Title:</span>
         <input
@@ -47,19 +43,25 @@ export default function CreateForm() {
           type="text"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
+          name="title"
         />
       </label>
       <label>
-        <span>Title:</span>
+        <span>Body:</span>
         <textarea
           required
           onChange={(e) => setBody(e.target.value)}
           value={body}
+          name="body"
         />
       </label>
       <label>
         <span>Priority:</span>
-        <select onChange={(e) => setPriority(e.target.value)} value={priority}>
+        <select
+          onChange={(e) => setPriority(e.target.value)}
+          value={priority}
+          name="priority"
+        >
           <option value="low">Low Priority</option>
           <option value="medium">Medium Priority</option>
           <option value="high">High Priority</option>
